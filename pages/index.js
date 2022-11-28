f34f5be02ab8dcfd51c090da8f5d5d1e5110b35e
joaocorner <joaoeduardocorner@gmail.com>
2022-11-28 20:36:22 -0300

    changing header style

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,17 @@ function HomePage() {
 
 export default HomePage;
 
-const StyledHeader = styled.div``;
+const StyledHeader = styled.div`
+  background-color: #212529;
+  color: #fff;
+  height: 100px;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  font-size: 1.5rem;
+`;
 
 const Header = () => {
-  return <StyledHeader>Header</StyledHeader>;
+  return <StyledHeader>ToDo List</StyledHeader>;
 };
