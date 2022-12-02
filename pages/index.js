dcf562727b19601a57e08215ce022e62b32cf3e1
joaocorner <joaoeduardocorner@gmail.com>
2022-12-01 22:35:56 -0300

    creating a card example

--- pages/index.js
+++ pages/index.js
@@ -92,6 +92,51 @@ const Main = () => {
     <StyledMain>
       <h1>ToDo List</h1>
       <p>Let's get some work done!</p>
+      <Card title="Task 1" description="This is a task" />
     </StyledMain>
+  );
+};
+
+const StyledCard = styled.div`
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 100%;
+  max-width: 500px;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: 1.2rem;
+    margin-bottom: 10px;
+  }
+  button {
+    background-color: #212529;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 1rem;
+    cursor: pointer;
+    &:hover {
+      background-color: #000;
+    }
+  }
+`;
+
+const Card = ({ title, description, id, deleteTask }) => {
+  return (
+    <StyledCard>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <button onClick={() => deleteTask(id)}>Delete</button>
+    </StyledCard>
   );
 };
